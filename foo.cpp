6cda57f4563d7f0aa676d083288f51b870e5bd7e
Suikan <26223147+suikan4github@users.noreply.github.com>
2024-09-13 22:06:12 +0900

    Clang fails to compile when using global resolution operator.

---
+++ foo.cpp
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+class foo {
+ public:
+  uint32_t bar(unsigned int a);
+  int baz(int a);
+};
+
+// When 1, fail to compiler by Clang 18.1.3 x86_64-pc-linux-gnu
+#if 1
+uint32_t ::foo::bar(unsigned int a) { return a; }
+#else
+uint32_t ::foo::bar(unsigned int a) { return a; }
+#endif
+
+int ::foo::baz(int a) { return a; }
+
+int main() {
+  foo my_foo;
+
+  return (my_foo.bar(0));
+}

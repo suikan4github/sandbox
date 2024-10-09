fce97b13518ff12ea3cd18eaf7ff0567bb88ebe8
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2024-10-10 08:34:41 +0900

    Renamed files.

---
+++ src/foo.cpp
@@ -0,0 +1,9 @@
+#include "foo.hpp"
+
+#include <assert.h>
+
+double Calc::sqrt(double param) {
+  assert(param >= 0);
+
+  return std::sqrt(param);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,21 @@
  */
 #include <iostream>
 #include <thread>
-#include "calc.hpp"
+
+#include "foo.hpp"
 
 /**
  * @brief Calculate the square root of 1 to the given num.
  *
  * @param num Max value to calc the square root
  */
-void CalcSqrt(int num)
-{
-    Calc calc;
+void CalcSqrt(int num) {
+  Calc calc;
 
-    // count from 1 to num
-    for (int i = 1; i <= num; i++)
-        std::cout << "[CalcSqrt] The square root of " << i << " is " << calc.sqrt(i) << std::endl;
+  // count from 1 to num
+  for (int i = 1; i <= num; i++)
+    std::cout << "[CalcSqrt] The square root of " << i << " is " << calc.sqrt(i)
+              << std::endl;
 }
 
 /**
@@ -31,19 +32,19 @@ void CalcSqrt(int num)
  *
  * @return int Always 0
  */
-int main()
-{
-    std::cout << "[main] Thread start." << std::endl;
+int main() {
+  std::cout << "[main] Thread start." << std::endl;
 
-    // run the CalcSaqrt() as thread. This is for the test of the std::thread functionality in the each platform
-    std::thread t(CalcSqrt, 10);
+  // run the CalcSaqrt() as thread. This is for the test of the std::thread
+  // functionality in the each platform
+  std::thread t(CalcSqrt, 10);
 
-    std::cout << "[main] Waiting for the end of thread." << std::endl;
+  std::cout << "[main] Waiting for the end of thread." << std::endl;
 
-    // wait for the end of thread execution.
-    t.join();
+  // wait for the end of thread execution.
+  t.join();
 
-    std::cout << "[main] Thread terminated." << std::endl;
+  std::cout << "[main] Thread terminated." << std::endl;
 
-    return 0;
+  return 0;
 }
